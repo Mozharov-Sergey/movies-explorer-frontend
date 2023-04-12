1d487576a462b88732eec8810c268166b445c564
Mozharov Sergey <mozharov-ss@yandex.ru>
2023-04-12 16:32:00 +0300

    rafector: переписать ProtectedRoute для использования с react-router-dom шестой версии

--- movies-frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++ movies-frontend/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import { Route, Navigate } from 'react-router-dom';
-
-// export default function ProtectedRoute({ component: Component, ...props }) {
-//   return (
-//     <Route path={props.path}>
-//       {() => (props.isLoggedIn ? <Component {...props} /> : <Navigate to="/" />)}
-//     </Route>
-//   );
-// }
-
-
 import React from 'react';
 import {Navigate} from 'react-router-dom';
 
